2a0e2cf1e69a241c82978719328f73bcd4e6f00b
Shubha Debnath <shubhd@amazon.com>
2022-10-17 20:32:17 -0700

    Find all end points

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
+import json
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Opening JSON file
+    f = open('./resource/swagger.json')
+
+    # Loading json content
+    data = json.load(f)
+
+    # Iterating API under paths
+    for i in data['paths']:
+        print(i)
+
+    # Closing file
+    f.close()
+
+
